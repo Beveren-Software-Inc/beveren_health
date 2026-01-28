0d39bfb5f9364dc31aafe81d3f90d19fd0661592
diwvish1407 <diwvish1407@gmail.com>
2026-01-28 22:53:57 +0300

    Item Master Customizations

---
+++ beveren_health/beveren_health/doctype/pharmacy_agent/pharmacy_agent.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Beveren Software and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Pharmacy Agent", {
+// 	refresh(frm) {
+
+// 	},
+// });
