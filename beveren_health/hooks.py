c09dda01031de75656206d623eb65550b1b21a72
maniamartial <martialmania19@gmail.com>
2026-03-02 14:53:18 +0300

    feat: add batch manufacturing date & expiry date from pos

--- beveren_health/hooks.py
+++ beveren_health/hooks.py
@@ -167,6 +167,9 @@ doc_events = {
     },
     "Shift Type": {
         "before_save": "beveren_health.beveren_health.customize.shift_type.before_save"
+    },
+    "Batch": {
+        "before_save": "beveren_health.beveren_health.override.batch.before_save"
     }
 }
 
@@ -301,6 +304,8 @@ fixtures = [
                 "Purchase Receipt Item-custom_label_print",
                 "Purchase Receipt Item-custom_label_printing",
                 "Item Barcode-custom_image",
+                "Purchase Receipt Item-custom_expiry_date",
+                "Purchase Receipt Item-custom_manufacturing_date",
             ]]
         ]
     }
