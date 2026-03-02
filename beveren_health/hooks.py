28e38a8d705adc09564a5a95c6d989e85434c9e0
maniamartial <martialmania19@gmail.com>
2026-03-02 16:26:10 +0300

    feat: Add crno also on the warehouse

--- beveren_health/hooks.py
+++ beveren_health/hooks.py
@@ -306,6 +306,8 @@ fixtures = [
                 "Item Barcode-custom_image",
                 "Purchase Receipt Item-custom_expiry_date",
                 "Purchase Receipt Item-custom_manufacturing_date",
+                "Cost Center-custom_cr_no",
+                "Warehouse-custom_cr_no"
             ]]
         ]
     }
